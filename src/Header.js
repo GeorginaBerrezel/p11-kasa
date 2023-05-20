59cd54fa3574a427c561ef18fad701adb7ce07b9
Georgina <georgina.berrezel@gmail.com>
2023-05-20 15:09:59 +0200

    Add header and home section components

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './kasa-logo.svg';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Logo Kasa" />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Accueil</a>
+          </li>
+          <li>
+            <a href="/a-propos">À propos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
