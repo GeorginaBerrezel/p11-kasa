0d8f99c9b69e539f83d730c855e5efb8531543ac
Georgina <georgina.berrezel@gmail.com>
2023-05-20 17:12:18 +0200

    Add thumbnail component to Home page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React from 'react';
-import Accommodation from './Accomodation';
 import Header from './Header';
-import SectionHome from './Sectionhome'; // Import du composant SectionHome
+import SectionHome from './Sectionhome';
+import Thumbnail from './Thumbnail';
+import jsonData from './logements.json';
 
 const App = () => {
-  const jsonData = [
-    {
-      "id": "c67ab8a7",
-      "title": "Appartement cosy",
-      "cover": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-      "pictures": [
-        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
-        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
-        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
-        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
-      ]
-    },
-    // Ajoutez d'autres objets de données ici
-  ];
+  const getGalleryItems = () => {
+    return jsonData.map((accommodation) => (
+      <Thumbnail
+        key={accommodation.id}
+        title={accommodation.title}
+        cover={accommodation.cover}
+      />
+    ));
+  };
 
   return (
     <div>
-      <Header /> {/* Ajout du composant Header */}
-      <SectionHome /> {/* Ajout du composant SectionHome */}
-      {jsonData.map((accommodation) => (
-        <Accommodation
-          key={accommodation.id}
-          title={accommodation.title}
-          cover={accommodation.cover}
-          pictures={accommodation.pictures}
-        />
-      ))}
+      <Header />
+      <SectionHome />
+
+      <div className="gallery-row">
+        {getGalleryItems()}
+      </div>
     </div>
   );
 };
