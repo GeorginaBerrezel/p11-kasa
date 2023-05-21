b21e2604baa0071f30c06cc07f63c5172ed07cd0
Georgina <georgina.berrezel@gmail.com>
2023-05-21 13:05:13 +0200

    Add footer and fixed nav to accommodation details

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
-import React from 'react';
-import Header from './Header';
-import SectionHome from './Sectionhome';
-import Thumbnail from './Thumbnail';
-import Accommodation from './Accommodation'; // Correction ici
-import { useState } from 'react';
-import jsonData from './logements.json';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from "./Header";
+import SectionHome from "./Sectionhome";
+import Thumbnail from "./Thumbnail";
+import Accommodation from "./Accommodation";
+import AccommodationDetails from "./AccommodationDetails";
+import { useState } from "react";
+import jsonData from "./logements.json";
+import Footer from "./Footer";
+
 
 const App = () => {
   const [selectedAccommodationId, setSelectedAccommodationId] = useState(null);
   const getGalleryItems = () => {
     return jsonData.map((accommodation) => (
-      <Thumbnail
-        key={accommodation.id}
-        title={accommodation.title}
-        cover={accommodation.cover}
-        onClick={() => setSelectedAccommodationId(accommodation.id)}
-      />
+      <Link key={accommodation.id} to={`/accommodation/${accommodation.id}`}>
+        <Thumbnail
+          title={accommodation.title}
+          cover={accommodation.cover}
+          onClick={() => setSelectedAccommodationId(accommodation.id)}
+        />
+      </Link>
     ));
   };
+  
 
   return (
-    <div>
-      <Header />
-      <SectionHome />
-
-      <div className="gallery-row">
-      {selectedAccommodationId ? (
-        <Accommodation id={selectedAccommodationId} />
-      ) : (
-        getGalleryItems()
-      )}
-    </div>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <SectionHome />
+  
+        <Routes>
+          <Route path="/" element={<div className="gallery-row">{selectedAccommodationId ? (
+            <Accommodation id={selectedAccommodationId} />
+          ) : (
+            getGalleryItems()
+          )}</div>} />
+  
+          <Route path="/accommodation/:id" element={<AccommodationDetails />} />
+        </Routes>
+  
+        <Footer />
+      </div>
+    </Router>
   );
+  
+  
 };
 
 export default App;
