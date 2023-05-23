7b910cfa80c91c850e1bda6c62a041bc96972050
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2023-05-23 18:31:45 +0200

    Started to fixed home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import './style.css';
+import Thumbnail from "../../components/Thumbnail/Thumbnail";
+import Accommodation from "../../components/Accommodation/Accommodation";
+import jsonData from "../../logements.json";
 
 export default function Home() {
-    return (
-        <div className="section-home">
-          <div className="section-content">
-            <h1>Chez vous, partout et ailleurs</h1>
-          </div>
-        </div>
-    );
+  const [selectedAccommodationId, setSelectedAccommodationId] = useState(null);
+
+  const getGalleryItems = () => {
+    return jsonData.map((accommodation) => (
+      <Link key={accommodation.id} to={`/accommodation/${accommodation.id}`}>
+        <Thumbnail
+          title={accommodation.title}
+          cover={accommodation.cover}
+          onClick={() => setSelectedAccommodationId(accommodation.id)}
+        />
+      </Link>
+    ));
+  };
+
+  return (
+    <div className="section-home">
+      <div className="section-content">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
+      <div className="gallery-row">
+        {selectedAccommodationId ? (
+          <Accommodation id={selectedAccommodationId} />
+        ) : (
+          getGalleryItems()
+        )}
+      </div>
+    </div>
+  );
 }
