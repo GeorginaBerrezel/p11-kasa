8808f5833c0258a7e2f0c16c3c1d83c6936b3646
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2023-07-17 16:43:24 +0200

    Add collapse component

--- src/pages/Accommodation/AccommodationDetails.js
+++ src/pages/Accommodation/AccommodationDetails.js
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import Carrousel from "../../components/Carrousel/Carrousel";
 import jsonData from "../../logements.json";
 import Sheet from "../../components/Sheet/Sheet";
+import Collapse from "../../components/Collapse/Collapse"; // Importer le composant Collapse
+
 
 const AccommodationDetails = () => {
   const { id } = useParams();
@@ -12,12 +14,17 @@ const AccommodationDetails = () => {
   if (!accommodation) {
     return <div>Aucun hébergement trouvé pour cet ID</div>;
   } else {
-    const { pictures } = accommodation; // Déplacer cette ligne ici
+    const { pictures, description, equipments } = accommodation; // Déplacer cette ligne ici
 
     return (
       <div>
         <Carrousel pictures={pictures} />
         <Sheet accommodation={accommodation} />
+        <div className="collapses">
+          <Collapse title="Description" content={description} /> {/* Ajouter le composant Collapse ici */}
+          <Collapse title="Équipements" content={equipments} />
+        </div>
+
       </div>
     );
   }
