9b21fb0d59c4952969b2dbf7b7a0dd879cf7fa4b
Georgina Berrezel <georgina.berrezel@gmail.com>
2023-07-22 14:08:29 +0200

    Create about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Collapse from "../../components/Collapse/Collapse";
+import aproposData from "../../apropos.json";
 
 export default function About() {
-    return  (
-        <div>
-          <div className="section-about-us">
-          </div>
-        </div>
-      );
-    
+  return (
+    <div>
+      <div className="section-about-us">
+        {/* Insérer ici le contenu de la div "section-about-us" */}
+      </div>
+      <div className="collapses-container">
+        {aproposData.map((item, index) => (
+          <Collapse key={index} title={item.title} content={item.content} />
+        ))}
+      </div>
+    </div>
+  );
 }
