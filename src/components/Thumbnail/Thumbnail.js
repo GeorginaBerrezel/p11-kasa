b30360d6d457b86cec19fe0a5a076d57dc88c69d
georgina berrezel <georgina.berrezel@gmail.com>
2023-08-20 02:02:53 +0200

    fixed home page responsive

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -4,14 +4,14 @@ import './style.css';
 function Thumbnail(props) {
     const { title, cover, onClick, id } = props;
 
-  return (
-    <div className="thumbnail" onClick={onClick}>
-    <h1>{id}</h1>
-      <img src={cover} alt={title} />
-      <h2 className="thumbnail-title">{title}</h2>
-    </div>
-  );
+    return (
+        <div className="thumbnail" onClick={onClick}>
+            <div className="overlay"></div>
+            <h1>{id}</h1>
+            <img src={cover} alt={title} />
+            <h2 className="thumbnail-title">{title}</h2>
+        </div>
+    );
 }
 
 export default Thumbnail;
-  
